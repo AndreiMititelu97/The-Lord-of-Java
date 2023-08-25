b0b98d8e2814274a99110fa794746b3af08c21e1
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-08-25 16:43:15 +0300

    Added Creatures class

---
+++ src/main/java/Andrei/Creatures.java
@@ -0,0 +1,7 @@
+package Andrei;
+
+public class Creatures {
+    private Abilities abilities; // Stats for a creature: Stamina, Speed and Agility
+    private nickname String; // Nickname of the creature
+    private long score; //Score of the creature
+}
