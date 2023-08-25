8468619022a74dc40b2b296c87d1c516640cb96a
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-08-25 16:44:09 +0300

    fixed nickname field

--- src/main/java/Andrei/Creatures.java
+++ src/main/java/Andrei/Creatures.java
@@ -2,6 +2,6 @@ package Andrei;
 
 public class Creatures {
     private Abilities abilities; // Stats for a creature: Stamina, Speed and Agility
-    private nickname String; // Nickname of the creature
+    private String nickname; // Nickname of the creature
     private long score; //Score of the creature
 }
