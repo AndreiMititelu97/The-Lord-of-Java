114cc0318e3c3c26661f9533aeb08340390d5dfd
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-08-25 16:46:33 +0300

    Revert "Added Creatures class"
    This reverts commit ece41b1f5210fa00cd5fb782663c25105a0d3db6.

--- src/main/java/Andrei/Creatures.java
+++
@@ -1,7 +0,0 @@
-package Andrei;
-
-public class Creatures {
-    private Abilities abilities; // Stats for a creature: Stamina, Speed and Agility
-    private nickname String; // Nickname of the creature
-    private long score; //Score of the creature
-}
