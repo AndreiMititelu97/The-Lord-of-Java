cb0cc1844d79fe3baa807839b47afa9a8eb80ca6
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-08-25 16:46:07 +0300

    Revert "fixed nickname field"
    This reverts commit 8468619022a74dc40b2b296c87d1c516640cb96a.

--- src/main/java/Andrei/Creatures.java
+++ src/main/java/Andrei/Creatures.java
@@ -2,6 +2,6 @@ package Andrei;
 
 public class Creatures {
     private Abilities abilities; // Stats for a creature: Stamina, Speed and Agility
-    private String nickname; // Nickname of the creature
+    private nickname String; // Nickname of the creature
     private long score; //Score of the creature
 }
