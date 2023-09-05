bd303220d552c1116443548b18ee2607422ee417
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-09-05 23:23:17 +0300

    Added asserEquals methods for each field of the class

--- src/test/java/ParametersTest.java
+++ src/test/java/ParametersTest.java
@@ -4,20 +4,28 @@ import org.junit.jupiter.api.Test;
 import Andrei.Parameters;
 @DisplayName("Test Parameter class")
 public class ParametersTest {
+    private final String os = "myOS";
+    private final String gamePATH = "myPath";
+    private final String credentials = "admin";
+    private final int noLives = 1;
 
     @Test
     void testConstructor(){
-        Parameters parameters = new Parameters("myOS", "myPath", "admin", 1);
+        Parameters parameters = new Parameters(os, gamePATH, credentials, noLives);
+        assertEquals(os, parameters.getOS());
+        assertEquals(gamePATH, parameters.getGamePATH());
+        assertEquals(credentials, parameters.getCredentials());
+        assertEquals(noLives, parameters.getNoLives());
     }
 
     @Test
     void testToString(){
-        Parameters parameters = new Parameters("myOS", "myPath", "admin", 1);
-        String parametersToString = parameters.toString();
-        String refToString = "OS: myOS\n" +
-                "Game Path: myPath\n" +
-                "Credentials: admin\n" +
-                "No. lives remaining: 1";
-        assertEquals(refToString, parametersToString);
+        Parameters parameters = new Parameters(os, gamePATH, credentials, noLives);
+        String refToString = String.format("OS: %s\n" +
+                "Game Path: %s\n" +
+                "Credentials: %s\n" +
+                "No. lives remaining: %d", os, gamePATH, credentials, noLives);
+
+        assertEquals(refToString, parameters.toString());
     }
 }
