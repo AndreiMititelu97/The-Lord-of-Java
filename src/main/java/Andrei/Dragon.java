fb89d22502586b248c14af8edeb6588cac658612
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-09-07 16:33:29 +0300

    Added Dragon, IBurn and CreatureTest

---
+++ src/main/java/Andrei/Dragon.java
@@ -0,0 +1,34 @@
+package Andrei;
+
+public class Dragon extends Creature implements IBurn{
+    private double flamesPower;
+    private int flySpeed;
+
+    public Dragon(double stamina, double speed, int agility, String nickname, long score, double flamesPower, int flySpeed){
+        super(stamina, speed, agility, nickname, score);
+        this.flamesPower = flamesPower;
+        this.flySpeed = flySpeed;
+    }
+
+    @Override
+    public double getFlamesPower(){
+        return flamesPower;
+    }
+
+    @Override
+    public int getFlyingSpeed(){
+        return flySpeed;
+    }
+
+    @Override
+    public void powerUp(double newStamina, double newSpeed, int newAgility){
+        this.getAbilities().update(2 * newStamina, newSpeed / 2, 3 * newAgility);
+    }
+
+    @Override
+    public String toString(){
+        return String.format(super.toString() +
+                "Flames power: %f\n" +
+                "Fly speed: %d", flamesPower, flySpeed);
+    }
+}
