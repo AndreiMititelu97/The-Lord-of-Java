fe53b6440808726286153156fa51ef0332349d79
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-09-08 08:31:27 +0300

    Refactor

--- src/test/java/ParametersTest.java
+++ src/test/java/ParametersTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import Andrei.Parameters;
 @DisplayName("Test Parameter class")
@@ -8,24 +9,18 @@ public class ParametersTest {
     private final String gamePATH = "myPath";
     private final String credentials = "admin";
     private final int noLives = 1;
+    private Parameters parameters;
+
+    @BeforeEach
+    void setUp(){
+        parameters = new Parameters(os, gamePATH, credentials, noLives);
+    }
 
     @Test
     void testConstructor(){
-        Parameters parameters = new Parameters(os, gamePATH, credentials, noLives);
         assertEquals(os, parameters.getOS());
         assertEquals(gamePATH, parameters.getGamePATH());
         assertEquals(credentials, parameters.getCredentials());
         assertEquals(noLives, parameters.getNoLives());
-    }
-
-    @Test
-    void testToString(){
-        Parameters parameters = new Parameters(os, gamePATH, credentials, noLives);
-        String refToString = String.format("OS: %s\n" +
-                "Game Path: %s\n" +
-                "Credentials: %s\n" +
-                "No. lives remaining: %d", os, gamePATH, credentials, noLives);
-
-        assertEquals(refToString, parameters.toString());
     }
 }
