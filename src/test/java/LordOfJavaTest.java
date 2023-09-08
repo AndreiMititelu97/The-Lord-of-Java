d50c59ee61274d6cb89c13c387dde4453b3bec41
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-09-08 14:31:27 +0300

    Added LordOfJava class and LordOfJava unit test

---
+++ src/test/java/LordOfJavaTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import Andrei.Dragon;
+import Andrei.Warg;
+import Andrei.Mumakil;
+import Andrei.LordOfJava;
+public class LordOfJavaTest {
+    private LordOfJava lordOfJava;
+
+    @BeforeEach
+    void setUp(){
+        lordOfJava = new LordOfJava("Mac OS", "./username", "strongPassword2021", 3);
+    }
+
+    @Test
+    void testBattleDragonsWargs(){
+        Dragon dragon81 = new Dragon(25, 100, 15, "master_dragon1", 80, 20, 40);
+        Dragon dragon82 = new Dragon(35, 76, 5, "master_dragon2", 100, 20, 40);
+
+        Warg warg81 = new Warg(25, 100, 15, "master_warg1", 85, 20);
+        Warg warg82 = new Warg(80, 56, 23, "master_warg1", 85, 20);
+
+        Mumakil mumakil81 = new Mumakil(25, 100, 15, "master_muma", 60);
+
+        lordOfJava.addCreature(dragon81, "25,25");
+        lordOfJava.addCreature(dragon82, "14,24");
+        lordOfJava.addCreature(warg81, "12,27");
+        lordOfJava.addCreature(warg82, "05,15");
+        lordOfJava.addCreature(mumakil81, "01,01");
+
+        assertEquals(-1, lordOfJava.battleDragonsWargs());
+    }
+}
